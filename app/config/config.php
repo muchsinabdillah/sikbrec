6b6f8bfca0dd1990733007c741c674a349c3910c
MOCHAMAD MUCHSIN ABDILLAH <70797619+muchsinabdillah@users.noreply.github.com>
2025-12-28 10:13:33 +0700

    s

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
-define('BASEURL', 'http://10.10.11.246:8080/SIKBREC/public');
-define('BASEURL_PHOTO_SIGNATURE', 'http://10.10.11.246:8080/SIKBREC/public');
+define('BASEURL', 'http://72.62.122.16:8002/SIKBREC/public');
+define('BASEURL_PHOTO_SIGNATURE', 'http://72.62.122.16:8002/SIKBREC/public');
 //ADD_BPJS
 //define('URL_BPJS', 'https://dvlp.bpjs-kesehatan.go.id/vclaim-rest-1.1/Peserta/');
 define('URL_BPJS', 'https://apijkn-dev.bpjs-kesehatan.go.id/vclaim-rest-dev/');
@@ -12,12 +12,12 @@ define('URL_BPJS_REFF', 'https://apijkn-dev.bpjs-kesehatan.go.id/vclaim-rest-dev
 define('URL_BPJS_ANTRIAN', 'https://apijkn-dev.bpjs-kesehatan.go.id/antreanrs_dev/');
 // define('URL_BPJS_ANTRIAN', 'https://apijkn.bpjs-kesehatan.go.id/antreanrs/');//online
 define('URL_BPJS_ICARE', 'https://apijkn-dev.bpjs-kesehatan.go.id/ihs_dev/api/rs/validate'); //online
-define('URL_BPJS_ANTRIAN_CENDANA', 'http://172.16.10.216:8080/yarsi/api/api/');
-define('URL_BPJS_ANTRIAN_RS_YARSI', 'http://172.16.40.12:8080/api/v1/');
+define('URL_BPJS_ANTRIAN_CENDANA', 'http://172.16.10.216:8002/yarsi/api/api/');
+define('URL_BPJS_ANTRIAN_RS_YARSI', 'http://172.16.40.12:8002/api/v1/');
 
 define('KODE_RS_BPJS', '0114R067');
 // define('URL_API_RS', 'https://api.rsyarsi.co.id/api/');
-define('URL_API_RS', 'http://10.10.11.246:8070/api/');
+define('URL_API_RS', 'http://72.62.122.16:8070/api/');
 // define('URL_API_RS', 'http://127.0.0.1:8000/api/');
 define('URL_API_MYSDI', 'http://202.51.196.89/api/');
 define('URL_API_KEMENKES', 'https://api-satusehat-stg.dto.kemkes.go.id/oauth2/v1/');
@@ -39,7 +39,7 @@ define('URL_MOBILE_RSYARSI', 'http://apimobile.rsyarsi.co.id/api/');
 define('URL_EKLAIM', 'http://172.16.40.211/E-Klaim/ws.php');
 
 
-define('DB_HOST', '10.10.11.246');
+define('DB_HOST', '72.62.122.16');
 define('DB_USER', 'sa');
 define('DB_PASSWORD', 'Brec_2024');
 define('DB_NAME', 'Apotik_V1.1SQL');
